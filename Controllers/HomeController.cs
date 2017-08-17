22e8b06083ac05bea94e9802a8f1e144d15532ad
Lee Brandt <leebrandt@gmail.com>
2017-08-16 19:40:17 -0500

    Logout with Javascript logout 'workaround'

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Http;
 
 namespace AspnetOkta.Controllers
 {
@@ -34,6 +39,18 @@ namespace AspnetOkta.Controllers
       return View();
     }
 
+    public async Task<IActionResult> Login()
+    {
+      await HttpContext.Authentication.ChallengeAsync();
+      return View();
+    }
+    public async Task<IActionResult> Logout()
+    {
+      await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+      await HttpContext.Authentication.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+      //return SignOut(CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+      return RedirectToAction("Index");      
+    }
     public IActionResult Error()
     {
       return View();
